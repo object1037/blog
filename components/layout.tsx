ad3450ee545b7e170b8046f8da173a93f56df048
object_1037 <yamagami.10370409@gmail.com>
2021-03-08 10:10:13 +0900

    modify file name

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import Header from '../components/header'
-import Footer from '../components/footer'
+import Footer from './Footer'
 
 export const siteTitle = "ゆるふわインターネット"
 
