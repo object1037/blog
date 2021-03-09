95180f4520cee7aa7f12c46c4f38508278b754ac
object_1037 <yamagami.10370409@gmail.com>
2021-03-09 18:17:36 +0900

    modify posts' link design

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home({
         {allPostsData.map(({ idN, title, description }) => (
           <li key={'idN'} className="my-3 bg-gray-100 dark:bg-gray-800 rounded-sm shadow-sm hover:shadow-lg h-30">
             <Link href={`/posts/${idN}`}>
-              <a className="flex flex-col h-24 m-4">
+              <a className="flex flex-col h-32 p-4">
                 <Date dateString={String(idN)} />
                 <span className="text-xl mb-4 text-gray-900 dark:text-gray-100">{title}</span>
                 <span className="truncate text-gray-900 dark:text-gray-100">{description}</span>
