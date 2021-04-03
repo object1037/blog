f704c5324f1a7114cdcac945f83647987128fb3b
object1037 <contact@object1037.dev>
2021-04-03 21:05:56 +0900

    delete google search console tag

--- components/layout.tsx
+++ components/layout.tsx
@@ -34,7 +34,6 @@ export default function Layout({
         <meta property="og:title" content={siteTitle} />
         <meta property="og:description" content={`${handleName}のブログ`} />
         <meta property="og:image" content="https://object1037.vercel.app/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
-        <meta name="google-site-verification" content="NE6TDGJcUUnRDHmhTZdMo8rFYwP97C3w8rYSXMI3wyo" />
       </Head>
       <Header />
       <main>{children}</main>
@@ -57,7 +56,6 @@ export default function Layout({
         <meta property="og:image" content="https://object1037.vercel.app/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism-tomorrow.min.css" />
         <title>{meta.title} | {siteTitle}</title>
-        <meta name="google-site-verification" content="NE6TDGJcUUnRDHmhTZdMo8rFYwP97C3w8rYSXMI3wyo" />
       </Head>
       <Header />
       <main className="flex flex-col max-w-3xl mx-auto p-10">
