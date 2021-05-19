0c6c713baccc596fe5bfbe369d004b3cbd3aaf08
object1037 <contact@object1037.dev>
2021-05-19 23:23:43 +0900

    add alt

--- components/im.tsx
+++ components/im.tsx
@@ -4,8 +4,9 @@ interface Info {
   src: string;
   w: string;
   h: string;
+  alt: string;
 }
 
 export default function Im(info: Info) {
-  return <div className="flex-grow-0"><Image src={info.src} width={info.w} height={info.h} /></div>
+  return <div className="flex-grow-0"><Image src={info.src} width={info.w} height={info.h} alt={info.alt} /></div>
 }
