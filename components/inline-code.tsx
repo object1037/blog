d2e2b46dacb6b29afbf78c6479da85d0f263187c
object1037 <contact@object1037.dev>
2021-05-22 22:22:02 +0900

    add mx to the inline-code

--- components/inline-code.tsx
+++ components/inline-code.tsx
@@ -1,3 +1,3 @@
 export default function Code({children}: {children: string}) {
-  return <code className="bg-gray-200 px-2 py-1 rounded dark:bg-gray-800">{children}</code>
+  return <code className="mx-1 bg-gray-200 px-2 py-1 rounded dark:bg-gray-800">{children}</code>
 }
