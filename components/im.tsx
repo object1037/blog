1131cbdd8217ba12dcae63409fa185134a1e9832
object1037 <contact@object1037.dev>
2021-06-19 14:37:42 +0900

    update image component

--- components/im.tsx
+++ components/im.tsx
@@ -10,9 +10,12 @@ export default function Im(info: Info) {
     <div className="flex flex-col items-center mt-6 mb-4 space-y-4">
       <div className="flex-grow-0">
         <Image 
-          src={info.src} 
+          src={String(info.src.src)}
+          width={info.src.width} 
+          height={info.src.height}
           alt={info.alt}
           placeholder="blur"
+          blurDataURL={info.src.blurDataURL}
         />
       </div>
       <p className="text-gray-500 dark:text-gray-400 text-sm">{info.cap}</p>
