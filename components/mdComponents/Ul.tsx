35ce63293f0e1c5125298c0e9d680483d4b490b7
object1037 <contact@object1037.dev>
2021-06-19 17:22:09 +0900

    update list style

--- components/mdComponents/Ul.tsx
+++ components/mdComponents/Ul.tsx
@@ -1,5 +1,5 @@
 export default function Ul(props: HTMLElement) {
   return (
-    <ul className="list-disc ml-6 py-2">{props.children}</ul>
+    <ul className="list-disc ml-6 py-1">{props.children}</ul>
   )
 }
