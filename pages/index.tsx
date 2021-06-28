6b14d145f845f87130f09bbcf785d8317a00d87a
object1037 <contact@object1037.dev>
2021-06-28 18:17:11 +0900

    update data fetching method
    dynamic importの代わりにrequire.contextsしてgetStaticPropsで流す

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,27 @@
 import Head from 'next/head'
 import Layout, { siteTitle, handleName } from '../components/layout'
 import { GetStaticProps } from 'next'
-import { getSortedPostsData } from '../lib/posts'
+import { getAllPostsData } from '../lib/posts'
 import PostCard from '../components/post-card'
-import { useEffect, useState } from 'react'
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
+  const allPostsDatas = getAllPostsData()
   return {
     props: {
-      allPostsData
+      allPostsDatas
     }
   }
 }
 
 export default function Home({
-  allPostsData
+  allPostsDatas
 }: {
-  allPostsData: {
-    id: number
+  allPostsDatas: {
+    date: string,
+    title: string,
+    description: string
   }[]
 }) {
-  const postsCount = allPostsData.length
-  let articles = new Array
-  let promises = new Array(postsCount)
-  let initArr = new Array(postsCount)
-  initArr.fill({title: "loading...", description: "loading...", date: ''})
-
-  const [metas, setMetas] = useState(initArr)
-
-  let updateFlag = false
-
-  useEffect(() => {
-    for (let i = 0; i < postsCount; i++) {
-      let fileName = allPostsData[i].id;
-      promises.push(import(`./posts/${fileName}.mdx`)
-        .then((mod) => {
-          articles.push(mod.meta)
-        })
-      )
-    }
-    Promise.all(promises).then(() => {
-      for (let j = 0; j < postsCount; j++) {
-        if (metas[j] != articles[j]) {
-          updateFlag = true
-        }
-      }
-      if (updateFlag) {
-        setMetas(articles)
-      }
-    })
-  }, [metas]);
-
-
   return (
     <Layout home>
       <Head>
@@ -61,8 +30,8 @@ export default function Home({
       <section className="flex flex-col justify-center">
         <h1 className="text-center text-xl pb-10 pt-12 text-gray-900 dark:text-gray-100">{handleName}のブログです</h1>
         <ul className="flex flex-col justify-center px-10 max-w-3xl mx-auto w-full">
-          {allPostsData.map(({id}, index) => (
-            <PostCard key={String(id)} id={id} title={metas[index].title} description={metas[index].description} />
+          {allPostsDatas.map(({date}, index) => (
+            <PostCard key={date} date={date} title={allPostsDatas[index].title} description={allPostsDatas[index].description} />
           ))}
         </ul>
       </section>
