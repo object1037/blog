6ccfc2d37734a1b9624f93fbe47e66b4b3b3a9e3
object1037 <contact@object1037.dev>
2021-06-29 22:29:33 +0900

    levelごとに表示を変える #131
    h3要素はtocElement[]で渡したい

--- components/toc.tsx
+++ components/toc.tsx
@@ -5,11 +5,21 @@ export default function ToC({tocElements}: {tocElements: tocElement[]}) {
     <>
     <section>
       <ol>
-        {tocElements.map((element) => (
-          <li key={element.scrollPos}>
-            <a href={`#${element.title}`}>{element.title}</a>
-          </li>
-        ))}
+        {tocElements.map((element) => {
+          if (element.level === "H2") {
+            return (
+              <li key={element.scrollPos}>
+                <a href={`#${element.title}`}>{element.title}</a>
+              </li>
+            )
+          } else {
+            return (
+              <li key={element.scrollPos} className="pl-4">
+                <a href={`#${element.title}`}>{element.title}</a>
+              </li>
+            )
+          }
+        })}
       </ol>
     </section>
     </>
