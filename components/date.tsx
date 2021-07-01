6da81d04c0da8d9e83807ccdc5033266ba6b2810
object1037 <contact@object1037.dev>
2021-07-02 00:06:37 +0900

    update Date component to use <time> tag

--- components/date.tsx
+++ components/date.tsx
@@ -2,5 +2,5 @@ export default function Date({ dateString }: { dateString: string }) {
   const year: string = dateString.substr(0, 4)
   const month: string= dateString.substr(4, 2)
   const day: string = dateString.substr(6, 2)
-  return <span>{year}/{month}/{day}</span>
+  return <time dateTime={`${year}-${month}-${day}`}>{year}/{month}/{day}</time>
 }
