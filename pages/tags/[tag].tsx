87d64f3a94379c9f375c0e50a3e52b1b03edcab5
object1037 <contact@object1037.dev>
2021-08-05 09:47:46 +0900

    存在しないタグのページにアクセスすると404を返す

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { tagData } from '../../lib/getAllPostsTags'
+import { metaData } from '../../components/articleLayout'
 import Layout from '../../components/layout'
 import Head from 'next/head'
 import { siteTitle, siteUrl } from '../../components/articleLayout'
@@ -7,11 +8,18 @@ import { GetStaticProps } from 'next'
 import { getAllTags } from '../../lib/getAllPostsTags'
 import PostCard from '../../components/post-card'
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const allTags = getAllTags()
+  const searchByTag = (element: tagData) => element.name === params.tag
+  if (allTags.findIndex(searchByTag) === -1) {
+    return {
+      notFound: true,
+    }
+  }
+  const postsWithTag = allTags[allTags.findIndex(searchByTag)].articles
   return {
     props: {
-      allTags
+      postsWithTag
     }
   }
 }
@@ -24,17 +32,29 @@ export async function getStaticPaths() {
 }
 
 export default function TagPage({
-  allTags
+  postsWithTag
 }: {
-  allTags: tagData[]
+  postsWithTag: metaData[]
 }) {
   const router = useRouter()
   const { tag } = router.query
 
-  const searchByTag = (element: tagData) => element.name === tag
-
   if (router.isFallback) {
-    return <div>Loading...</div>
+    return (
+      <Layout>
+      <Head>
+        <meta name="description" content={`「${tag}」に関する記事一覧`} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@object1037" />
+        <meta property="og:url" content={`${siteUrl}/tags/${tag}`} />
+        <meta property="og:title" content={`「${tag}」に関する記事一覧`} />
+        <meta property="og:description" content={`「${tag}」に関する記事一覧`} />
+        <title>「{tag}」に関する記事一覧 | {siteTitle}</title>
+      </Head>
+      <h1 className="text-center text-xl pb-10 pt-12 text-gray-900 dark:text-gray-100">「 {tag} 」に関する記事一覧</h1>
+      <h2 className="text-center">Loading...</h2>
+    </Layout>
+    )
   }
 
   return (
@@ -50,7 +70,7 @@ export default function TagPage({
       </Head>
       <h1 className="text-center text-xl pb-10 pt-12 text-gray-900 dark:text-gray-100">「 {tag} 」に関する記事一覧</h1>
       <ul className="flex flex-col justify-center px-10 max-w-3xl mx-auto w-full">
-        {allTags[allTags.findIndex(searchByTag)].articles.map((meta) => (
+        {postsWithTag.map((meta) => (
           <PostCard key={meta.date} date={meta.date} title={meta.title} description={meta.description} />
         ))}
       </ul>
