f83129de8421521f4965a60fac4995b697f3ad33
object1037 <contact@object1037.dev>
2021-08-05 10:18:08 +0900

    change tag color

--- components/tag.tsx
+++ components/tag.tsx
@@ -7,7 +7,7 @@ export default function Tag({
 }) {
   return (
     <Link href={`/tags/${name}`}>
-      <a className="bg-gray-200 dark:bg-gray-700 px-4 py-2 m-2 rounded-md">
+      <a className="bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 px-4 py-2 m-2 rounded-md">
         # {name}
       </a>
     </Link>
