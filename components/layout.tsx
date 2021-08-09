427e2c8e62a919748296b77858813c209572239e
object1037 <contact@object1037.dev>
2021-08-09 16:06:57 +0900

    udpate layout component
    ogp関連

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,30 @@
 import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
+import { siteTitle, handleName } from './articleLayout'
 
 export default function Layout({
+  title = siteTitle,
+  description = `${handleName}のブログです`,
+  imgUrl = "https://object1037.dev/images/profile.jpg",
   children,
 }: {
+  title?: string,
+  description?: string,
+  imgUrl?: string,
   children: React.ReactNode,
 }) {
   return (
     <>
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:image" content="https://object1037.dev/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
+      <meta name="description" content={description} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@object1037" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imgUrl} />
+      <title>{title}</title>
     </Head>
     <Header />
     <main className="post-area mb-20">
