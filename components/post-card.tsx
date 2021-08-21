ea07cd804e5884b63c3925c47e936fd6207c7747
object1037 <contact@object1037.dev>
2021-08-21 20:50:14 +0900

    update postCard component design

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -4,7 +4,7 @@ import { metaData } from './articleLayout'
 
 export default function PostCard(meta: metaData) {
   return (
-    <li className="my-3 bg-gray-100 dark:bg-gray-800 rounded-sm shadow-sm hover:shadow-lg h-30">
+    <li className="my-3 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm hover:shadow-lg h-30 border border-transparent dark:hover:border-gray-600 transition">
       <Link href={`/posts/${meta.date}`}>
         <a className="flex flex-col h-32 p-4">
           <span className="text-xs text-gray-800 dark:text-gray-200"><DateDisplay dateString={meta.date} /></span>
