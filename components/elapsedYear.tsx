005901349cce30a7c45350a583112da03a01045f
object1037 <contact@object1037.dev>
2021-08-25 16:11:11 +0900

    update css

--- components/elapsedYear.tsx
+++ components/elapsedYear.tsx
@@ -5,21 +5,16 @@ export default function ElapsedYear({
 }: {
   yearNum: number
 }) {
-  let elapsedYearStyle = [
-    'hidden',
+  const elapsedYearStyle = [
+    'bg-rose-100',
+    'dark:bg-pink-900',
+    'p-5',
+    'mt-2',
+    'font-semibold',
+    'rounded-md',
   ]
-  if (yearNum > 0) {
-    elapsedYearStyle = [
-      'bg-rose-100',
-      'dark:bg-pink-900',
-      'p-5',
-      'mt-2',
-      'font-semibold',
-      'rounded-md'
-    ]
-  }
   return (
-    <div className={clsx(elapsedYearStyle)}>
+    <div className={clsx(yearNum === 0 ? 'hidden' : elapsedYearStyle)}>
       この記事は公開から{yearNum}年以上経過しています
     </div>
   )
