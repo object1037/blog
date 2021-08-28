2655c63ef1aa7681308f960c475aca6ba256b13d
object1037 <contact@object1037.dev>
2021-08-28 12:10:33 +0900

    rounded image component

--- components/im.tsx
+++ components/im.tsx
@@ -1,24 +1,36 @@
 import Image, { ImageProps } from 'next/image'
-interface Info {
-  src: ImageProps;
-  alt: string;
-  cap?: string;
-}
+import clsx from 'clsx'
 
-export default function Im(info: Info) {
+export default function Im({
+  src,
+  alt,
+  cap,
+}: {
+  src: ImageProps,
+  alt: string,
+  cap?: string
+}) {
+  const imWrapperStyle = [
+    'flex',
+    'overflow-hidden',
+    'rounded',
+    'border-0',
+    'mt-6',
+  ]
   return (
-    <div className="flex flex-col items-center mt-6 mb-4 space-y-4">
-      <div className="flex-grow-0">
+    <>
+      <div className={clsx(imWrapperStyle, !cap && 'mb-6')}>
         <Image 
-          src={String(info.src.src)}
-          width={info.src.width} 
-          height={info.src.height}
-          alt={info.alt}
+          src={String(src.src)}
+          width={src.width}
+          height={src.height}
+          alt={alt}
           placeholder="blur"
-          blurDataURL={info.src.blurDataURL}
+          blurDataURL={src.blurDataURL}
+          className="rounded overflow-hidden"
         />
       </div>
-      <p className="text-gray-500 dark:text-gray-400 text-sm">{info.cap}</p>
-    </div>
+      {cap ? <p className="text-gray-500 dark:text-gray-400 text-sm mt-4 mb-6 text-center">{cap}</p> : null}
+    </>
   )
 }
