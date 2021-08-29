c80bbcb06435a6b3cff8c4c088a8babafbe622e6
object1037 <contact@object1037.dev>
2021-08-29 20:33:16 +0900

    update header style

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,25 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import clsx from 'clsx'
+import profileImg from '../public/images/profile.jpg'
 
 export default function Header () {
+  const headerStyle = [
+    'px-6',
+    'sm:px-12',
+    'border-b',
+    'border-gray-200',
+    'dark:border-gray-700',
+    'text-gray-900',
+    'dark:text-gray-100',
+  ]
   return (
-    <header className="px-6 sm:px-12 border-b border-gray-300 dark:border-gray-600 text-gray-900 dark:text-gray-100">
+    <header className={clsx(headerStyle)}>
       <div className="flex flex-col md:flex-row items-stretch justify-start max-w-5xl mx-auto">
         <Link href="/">
           <a className="h-full py-3 md:py-4 flex flex-row items-center justify-center space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800 px-3">
-            <div className="rounded-full shadow-lg w-12 h-12">
-              <Image priority src="/images/profile.jpg" alt="picture of me" width={48} height={48} className="rounded-full" />
+            <div className="rounded-full overflow-hidden shadow-lg w-12 h-12">
+              <Image priority src={profileImg} alt="picture of me" className="rounded-full" placeholder="blur"/>
             </div>
             <span>ゆるふわインターネット</span>
           </a>
