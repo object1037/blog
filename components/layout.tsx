e465cefd9c823954d3b37e988d62205de6828c00
object1037 <contact@object1037.dev>
2021-08-29 11:40:38 +0900

    update h1 style

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,12 +8,14 @@ export default function Layout({
   description = `${handleName}のブログです`,
   imgUrl = "https://object1037.dev/images/profile.jpg",
   url = siteUrl,
+  h1,
   children,
 }: {
   title?: string,
   description?: string,
   imgUrl?: string,
   url?: string,
+  h1: string,
   children: React.ReactNode,
 }) {
   return (
@@ -30,8 +32,9 @@ export default function Layout({
       <title>{title}</title>
     </Head>
     <Header />
-    <main className="post-area mb-20">
-      <section className="flex flex-col justify-center">
+    <main className="post-area mb-20 px-6 sm:px-12">
+      <section className="flex flex-col justify-center items-center max-w-4xl mx-auto">
+        <h1 className="self-start text-4xl font-bold pb-10 pt-12 text-gray-900 dark:text-gray-100">{h1}</h1>
         {children}
       </section>
     </main>
