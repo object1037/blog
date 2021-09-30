91a3bdd0fc84cea008738c667887dec1a165c2e7
object1037 <contact@object1037.dev>
2021-09-30 17:37:38 +0900

    update link color to improve contrast

--- components/mdComponents/Anchor.tsx
+++ components/mdComponents/Anchor.tsx
@@ -1,6 +1,6 @@
 export default function Anchor(props: HTMLAnchorElement) {
   return (
-    <a target="_blank" rel="noopener noreferrer" className="text-blue-500 dark:text-blue-300 hover:underline" href={props.href}>
+    <a target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-300 hover:underline" href={props.href}>
       {props.children}
     </a>
   )
