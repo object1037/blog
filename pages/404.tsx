deadbc0beee3a5ecb453f2a334918f79c88418db
object1037 <contact@object1037.dev>
2021-09-30 18:23:58 +0900

    update link color

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,8 +15,8 @@ export default function Custom404() {
       <title>{`404 | ${siteTitle}`}</title>
     </Head>
     <main className="flex flex-col justify-center py-6">
-      <div className="circle relative rounded-full border-ppink border-solid w-60 h-60 mx-auto my-8">
-        <div className="errorCode flex items-center justify-center z-50 animate-none absolute transform translate-y-2 -translate-x-px inset-0 text-center align-baseline tracking-tighter text-gray-900 dark:text-ppink">404</div>
+      <div className="circle relative rounded-full border-ppink-200 border-solid w-60 h-60 mx-auto my-8">
+        <div className="errorCode flex items-center justify-center z-50 animate-none absolute transform translate-y-2 -translate-x-px inset-0 text-center align-baseline tracking-tighter text-gray-900 dark:text-ppink-200">404</div>
         <div className="bar absolute h-6 bg-white dark:bg-gray-900">
           <div className="triangle absolute bottom-full border-solid w-0 h-0"></div>
           <div className="triangle2 absolute top-full border-solid w-0 h-0"></div>
