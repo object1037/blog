661c9628a8efaf4aa436bada8f4610600dccc5ca
object1037 <contact@object1037.dev>
2021-09-30 17:54:33 +0900

    update Anchor component

--- components/mdComponents/Anchor.tsx
+++ components/mdComponents/Anchor.tsx
@@ -1,4 +1,9 @@
-export default function Anchor(props: HTMLAnchorElement) {
+export interface AnchorProps {
+  href: string
+  children: string
+}
+
+export default function Anchor(props: AnchorProps) {
   return (
     <a target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-300 hover:underline" href={props.href}>
       {props.children}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,28 +1,17 @@
 import Layout from '../components/layout'
 import { siteTitle, handleName, siteUrl } from '../components/articleLayout'
-import clsx from 'clsx'
+import A from '../components/mdComponents/Anchor'
 
 export default function About() {
-  const linkStyle = [
-    'text-blue-500',
-    'dark:text-blue-300',
-    'hover:underline'
-  ]
-  
   return (
     <>
     <Layout h1="About" title={`このブログについて | ${siteTitle}`} description={`このブログについて | ${handleName}のブログ`} url={`${siteUrl}/about`}>
       <div className="w-full">
         <p className="mb-6">
-          <a href="https://object1037.dev" target="_blank" rel="noopener noreferrer" className={clsx(linkStyle)}>
-            {handleName}
-          </a>
-          のブログです。
+          <A href="https://object1037.dev">{handleName}</A>のブログです。
         </p>
         <p>
-          <a href="https://github.com/object1037/blog" target="_blank" rel="noopener noreferrer" className={clsx(linkStyle)}>
-            Source
-          </a>
+          <A href="https://github.com/object1037/blog">Source</A>
         </p>
       </div>
     </Layout>
