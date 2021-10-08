b95a60eef566ff3c4970c8524674ba77bd35db5d
object1037 <contact@object1037.dev>
2021-10-08 16:19:48 +0900

    interface定義をまとめる

--- components/mdComponents/Anchor.tsx
+++ components/mdComponents/Anchor.tsx
@@ -1,9 +1,9 @@
-export interface AnchorProps {
+export interface anchorProps {
   href: string
   children: string
 }
 
-export default function Anchor(props: AnchorProps) {
+export default function Anchor(props: anchorProps) {
   return (
     <a target="_blank" rel="noopener noreferrer" className="text-ppink-300 dark:text-ppink-200 hover:underline" href={props.href}>
       {props.children}
