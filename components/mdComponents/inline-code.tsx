6c9d2bf980a7ecd3419a0b6a0654281aa160ebc0
object1037 <contact@object1037.dev>
2021-10-11 23:00:27 +0900

    fix word-break

--- components/mdComponents/inline-code.tsx
+++ components/mdComponents/inline-code.tsx
@@ -1,3 +1,3 @@
 export default function InlineCode(props: HTMLElement) {
-  return <code className="mx-1 bg-gray-200 px-2 py-1 rounded-md dark:bg-gray-800">{props.children}</code>
+  return <code className="mx-1 bg-gray-100 px-2 py-1 rounded-md dark:bg-gray-800 break-all">{props.children}</code>
 }
