34b02928d45ef6d9aa6c1b97a37aab9b11c8ea6e
object1037 <contact@object1037.dev>
2021-12-18 01:54:11 +0900

    polish search style

--- components/search.tsx
+++ components/search.tsx
@@ -57,25 +57,21 @@ export default function Search() {
     }
   }) {
     const cardStyle = [
-      'mb-6',
-      'bg-gray-100',
-      'dark:bg-gray-800',
-      'rounded-md',
-      'hover:bg-gray-200',
+      'hover:bg-gray-100',
       'dark:hover:bg-gray-800',
-      'dark:hover:border-gray-600',
-      'border',
-      'border-transparent',
+      'border-b',
+      'border-gray-300',
+      'dark:border-gray-600',
       'transition',
-      'px-6',
-      'py-7',
+      'px-5',
+      'py-6',
       'w-full'
     ]
     return (
       <>
       <Link href={`/posts/${hit.objectID}`} >
         <a className={clsx(cardStyle)}  onClick={() => closeModal()}>
-          <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-4 truncate">{hit.title}</h2>
+          <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-3 truncate">{hit.title}</h2>
           <p className="text-gray-900 dark:text-gray-100 truncate">{hit.description}</p>
         </a>
       </Link>
