f454954ed090afee952eaaaf816ee5baf059084f
object1037 <contact@object1037.dev>
2021-12-17 20:36:18 +0900

    update generateSearchIndex

--- lib/generateSearchIndex.ts
+++ lib/generateSearchIndex.ts
@@ -10,13 +10,17 @@ export default async function generateSearchIndex({
   if (frontmatter.draft) {
     return
   }
+  if (!process.env.ALGOLIA_APPLICATION_ID || !process.env.ALGOLIA_ADMIN_API_KEY) {
+    console.error("Algolia API keys are not found")
+    return
+  }
   const record = {
     objectID: frontmatter.date,
     title: frontmatter.title,
     description: frontmatter.description,
     content: plaintext
   }
-  const client = algoliasearch(process.env.ALGOLIA_APPLICATION_ID!, process.env.ALGOLIA_ADMIN_API_KEY!)
+  const client = algoliasearch(process.env.ALGOLIA_APPLICATION_ID, process.env.ALGOLIA_ADMIN_API_KEY)
   const index = client.initIndex('blog_datas')
   index.saveObject(record).then(() => {
     console.log(`Added post ${frontmatter.date} to the index`)
