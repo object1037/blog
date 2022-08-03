2f5fac21a23d96db63f93d55b949d51fbc59b58e
object1037 <contact@object1037.dev>
2022-08-03 19:02:30 +0900

    update about page and share component

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,16 +3,32 @@ import Layout from '../components/layout'
 import PageLayout from '../components/pageLayout'
 import { siteTitle, handleName, siteUrl, homepage, accounts } from '../constants/data'
 import { Anchor } from '../components/mdComponents'
+import { FiCode, FiRss } from 'react-icons/fi'
+import clsx from 'clsx'
 
 export default function Page() {
+  const linkStyle = [
+    'p-2',
+    'text-ngray-500',
+    'hover:text-black',
+    'dark:text-ngray-400',
+    'dark:hover:text-white',
+    'transition',
+  ]
+
   return (
     <div className="w-full">
       <p className="mb-6">
         <Anchor href={homepage}>{handleName}</Anchor>のブログです。
       </p>
-      <p>
-        <Anchor href={`https://github.com/${accounts.github}/blog`}>Source</Anchor> ・ <Anchor href={`${siteUrl}/feed.xml`}>RSS</Anchor>
-      </p>
+      <div className='flex space-x-2 text-xl mt-12'>
+        <a href={`https://github.com/${accounts.github}/blog`} className={clsx(linkStyle)} target="_blank" rel="noopener noreferrer" aria-label="Source code of this blog">
+          <FiCode />
+        </a>
+        <a href={`${siteUrl}/feed.xml`} className={clsx(linkStyle)} target="_blank" rel="noopener noreferrer" aria-label="RSS of this blog">
+          <FiRss />
+        </a>
+      </div>
     </div>
   )
 }
