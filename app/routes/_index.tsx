a4c59846041652ed5723c38557be9c2e38d25fe7
object1037 <contact@object1037.dev>
2023-10-18 21:01:24 +0900

    set up prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,12 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction } from '@remix-run/cloudflare'
 import { css } from 'styled-system/css'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
 
 export default function Index() {
   return (
