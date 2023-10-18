3992fc308362e8abbaf7860b926228e338ef4423
object1037 <contact@object1037.dev>
2023-10-19 00:02:39 +0900

    add markdown-it and patched it

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,11 +1,18 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
 import { css } from 'styled-system/css'
+import MarkdownIt from 'markdown-it'
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'New Remix App' },
     { name: 'description', content: 'Welcome to Remix!' },
   ]
+}
+
+export const loader = async () => {
+  const md = new MarkdownIt()
+  const result = md.render('# Hello World')
+  return { result }
 }
 
 export default function Index() {
