de2da6cc7a0ae492982195dd16b2ddc643436485
object1037 <contact@object1037.dev>
2023-10-22 02:03:44 +0900

    fix logout route

---
+++ app/routes/_auth.logout.tsx
@@ -0,0 +1,9 @@
+import { type ActionFunctionArgs } from '@remix-run/cloudflare'
+
+import { envSchema } from '~/env'
+import { getAuthenticator } from '~/services/auth.server'
+
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+  const authenticator = getAuthenticator(envSchema.parse(context.env))
+  await authenticator.logout(request, { redirectTo: '/login' })
+}
--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,22 +1,13 @@
-import { type ActionFunctionArgs } from '@remix-run/cloudflare'
 import { Form, Outlet } from '@remix-run/react'
-
-import { envSchema } from '~/env'
-import { getAuthenticator } from '~/services/auth.server'
 
 export default function AuthLayout() {
   return (
     <div>
       <h1>Auth Layout</h1>
-      <Form method="post">
+      <Form action="logout" method="post">
         <button>Logout</button>
       </Form>
       <Outlet />
     </div>
   )
-}
-
-export const action = async ({ request, context }: ActionFunctionArgs) => {
-  const authenticator = getAuthenticator(envSchema.parse(context.env))
-  await authenticator.logout(request, { redirectTo: '/login' })
 }
