37871b41e0fa4a73448495e9bbf2099b84b5c1f9
object1037 <contact@object1037.dev>
2023-10-21 18:25:14 +0900

    redirect to dashboard when logged in

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,4 +1,16 @@
+import { type LoaderFunctionArgs } from '@remix-run/cloudflare'
 import { Form } from '@remix-run/react'
+
+import { envSchema } from 'env'
+
+import { getAuthenticator } from '~/services/auth.server'
+
+export const loader = async ({ context, request }: LoaderFunctionArgs) => {
+  const authenticator = getAuthenticator(envSchema.parse(context.env))
+  return await authenticator.isAuthenticated(request, {
+    successRedirect: '/dashboard',
+  })
+}
 
 export default function Login() {
   return (
