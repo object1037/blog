0501e39f5b8e93f8f474b3418ef8d906c7345075
object1037 <contact@object1037.dev>
2023-10-24 09:55:57 +0900

    split styles

--- app/routes/_blog._index/postCard.tsx
+++ app/routes/_blog._index/postCard.tsx
@@ -1,9 +1,25 @@
 import { Link } from '@remix-run/react'
 
-import { css } from 'styled-system/css'
+import { css, cx } from 'styled-system/css'
 import { flex } from 'styled-system/patterns'
 
 import { type getPosts } from '~/db.server'
+
+const itemStyle = css({
+  h: '24',
+  borderColor: 'gray.900',
+  borderBottomWidth: { base: '1px', _last: '0px' },
+})
+const linkStyle = flex({
+  direction: 'column',
+  justify: 'space-between',
+  h: '100%',
+  p: '4',
+})
+const titleStyle = css({
+  fontWeight: 'medium',
+  fontSize: 'xl',
+})
 
 export const PostCard = ({
   post,
@@ -11,26 +27,9 @@ export const PostCard = ({
   post: Awaited<ReturnType<typeof getPosts>>[number]
 }) => {
   return (
-    <li
-      className={css({
-        h: '24',
-        borderColor: 'gray.900',
-        borderBottomWidth: { base: '1px', _last: '0px' },
-      })}
-    >
-      <Link
-        to={`/posts/${post.id}`}
-        prefetch="intent"
-        className={flex({
-          direction: 'column',
-          justify: 'space-between',
-          h: '100%',
-          p: '4',
-        })}
-      >
-        <p className={css({ fontWeight: 'medium', fontSize: 'xl' })}>
-          {post.title}
-        </p>
+    <li className={cx('group', itemStyle)}>
+      <Link to={`/posts/${post.id}`} prefetch="intent" className={linkStyle}>
+        <p className={titleStyle}>{post.title}</p>
         <p>{post.description}</p>
       </Link>
     </li>
