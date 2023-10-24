12a7afd178df6d3ed5092a28267fef09c320db2a
object1037 <contact@object1037.dev>
2023-10-24 15:44:53 +0900

    add back home link

--- app/routes/_blog.tsx
+++ app/routes/_blog.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from '@remix-run/react'
+import { Link, Outlet } from '@remix-run/react'
 
 export default function BlogLayout() {
   return (
     <div>
       <h1>Blog Layout</h1>
+      <Link to="/">Home</Link>
       <Outlet />
     </div>
   )
