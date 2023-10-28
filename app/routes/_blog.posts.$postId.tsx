230a83e5f88fccd05cd0f9d5f60b4aa36c1ff3a5
object1037 <contact@object1037.dev>
2023-10-29 01:29:44 +0900

    delete debug tag

--- app/routes/_blog.posts.$postId.tsx
+++ app/routes/_blog.posts.$postId.tsx
@@ -40,7 +40,6 @@ export default function Post() {
 
   return (
     <ContainerWithHeading heading={post.title}>
-      <h5>hoge</h5>
       <TagList tags={post.tags} />
       <div
         className="markdown_wrapper"
