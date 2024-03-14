ec52c646cb6ba2f16d6993fda7407a1ba742c1da
object1037 <contact@object1037.dev>
2024-03-14 12:54:08 +0900

    make it client only

--- app/components/editor.tsx
+++ app/components/editor.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react'
 
 import { type ViewUpdate } from '@codemirror/view'
 
-import { useEditor } from '~/utils/useEditor'
+import { useEditor } from '~/utils/useEditor.client'
 
 export const Editor = ({
   value,
