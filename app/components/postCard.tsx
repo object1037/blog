74f791021892ebe5ec4bcdbb039184952a7b1339
object1037 <contact@object1037.dev>
2024-03-28 17:53:22 +0900

    update postCard design
    make id vertical

--- app/components/postCard.tsx
+++ app/components/postCard.tsx
@@ -1,6 +1,6 @@
 import { Link } from '@remix-run/react'
 
-import { css } from 'styled-system/css'
+import { css, cx } from 'styled-system/css'
 import { flex } from 'styled-system/patterns'
 
 import { type getPosts } from '~/.server/db'
@@ -19,7 +19,7 @@ export const PostCard = ({
   })
   const linkStyle = flex({
     justify: 'space-between',
-    gap: '4',
+    gap: '2',
     p: '4',
   })
   const titleStyle = css({
@@ -30,10 +30,23 @@ export const PostCard = ({
   const idStyle = css({
     fontFamily: 'mono',
     fontSize: 'sm',
+    color: 'neutral.700',
+    writingMode: 'vertical-rl',
+    borderLeftWidth: '[1px]',
+    borderStyle: 'dashed',
+    borderColor: 'neutral.200',
+    mx: '-2',
+    px: '2',
+    pl: '3.5',
+    transition: 'colors',
+    _groupHover: {
+      borderStyle: 'solid',
+      borderColor: 'neutral.400',
+    },
   })
 
   return (
-    <li className={itemStyle}>
+    <li className={cx('group', itemStyle)}>
       <Link to={`/posts/${post.id}`} prefetch="viewport" className={linkStyle}>
         <div>
           <p className={titleStyle}>{post.title}</p>
