82a97ffb1d309a7cd450f05345f902326c7fd6c2
object1037 <contact@object1037.dev>
2025-08-16 22:47:45 +0900

    separate highlighter

--- app/islands/editor.tsx
+++ app/islands/editor.tsx
@@ -1,88 +1,12 @@
-import { useEffect, useRef, useState } from 'hono/jsx'
-import Prism from 'prismjs'
-import 'prismjs/components/prism-markdown'
 import { css } from 'hono/css'
-
-export const tokenTypes = [
-  'atrule',
-  'attr-name',
-  'attr-value',
-  'bold',
-  'boolean',
-  'builtin',
-  'cdata',
-  'char',
-  'class-name',
-  'comment',
-  'constant',
-  'deleted',
-  'doctype',
-  'entity',
-  'function',
-  'important',
-  'inserted',
-  'italic',
-  'keyword',
-  'namespace',
-  'number',
-  'operator',
-  'prolog',
-  'property',
-  'punctuation',
-  'regex',
-  'rule',
-  'selector',
-  'string',
-  'symbol',
-  'tag',
-  'url',
-]
+import { useEffect, useRef, useState } from 'hono/jsx'
+import { highlight, initHighlighter } from '../lib/highlight'
 
 const handleKeydown = (e: KeyboardEvent) => {
   if (e.code === 'Tab') {
     document.execCommand('insertHTML', false, '  ')
     e.preventDefault()
   }
-}
-
-const paintTokenHighlights = (
-  codeBlock: HTMLElement,
-  tokens: (string | Prism.Token)[],
-) => {
-  let pos = 0
-  for (const token of tokens) {
-    if (typeof token !== 'string') {
-      const range = new Range()
-      if (!codeBlock.firstChild) {
-        return
-      }
-      range.setStart(codeBlock.firstChild, pos)
-      range.setEnd(codeBlock.firstChild, pos + token.length)
-
-      const alias = token.alias
-      const alias_str = Array.isArray(alias) ? alias[0] : alias
-      CSS.highlights.get(alias_str ?? token.type)?.add(range)
-    }
-    pos += token.length
-  }
-}
-
-const highlight = (codeBlock: HTMLElement | null, content: string) => {
-  if (!codeBlock) return
-
-  const mdGrammer = Prism.languages.markdown
-  if (!mdGrammer) {
-    console.error('Markdown grammar not found')
-    console.log(Prism.languages)
-    return
-  }
-  const tokens = Prism.tokenize(content, mdGrammer)
-
-  tokenTypes.forEach((tokenType) => {
-    CSS.highlights.get(tokenType)?.clear()
-  })
-
-  paintTokenHighlights(codeBlock, tokens)
 }
 
 const containerStyle = css`
@@ -132,10 +56,7 @@ export const Editor = ({ initialValue = '' }: { initialValue?: string }) => {
   const [content, setContent] = useState(initialValue)
 
   useEffect(() => {
-    Prism.manual = true
-    tokenTypes.forEach((tokenType) => {
-      CSS.highlights.set(tokenType, new Highlight())
-    })
+    initHighlighter()
     updateTextareaHeight()
   }, [])
 
---
+++ app/lib/highlight.ts
@@ -0,0 +1,85 @@
+import Prism from 'prismjs'
+import 'prismjs/components/prism-markdown'
+
+const paintTokenHighlights = (
+  codeBlock: HTMLElement,
+  tokens: (string | Prism.Token)[],
+) => {
+  let pos = 0
+  for (const token of tokens) {
+    if (typeof token !== 'string') {
+      const range = new Range()
+      if (!codeBlock.firstChild) {
+        return
+      }
+      range.setStart(codeBlock.firstChild, pos)
+      range.setEnd(codeBlock.firstChild, pos + token.length)
+
+      const alias = token.alias
+      const alias_str = Array.isArray(alias) ? alias[0] : alias
+      CSS.highlights.get(alias_str ?? token.type)?.add(range)
+    }
+    pos += token.length
+  }
+}
+
+export const initHighlighter = () => {
+  Prism.manual = true
+  tokenTypes.forEach((tokenType) => {
+    CSS.highlights.set(tokenType, new Highlight())
+  })
+  console.log('Highlighter initialized')
+}
+
+export const highlight = (codeBlock: HTMLElement | null, content: string) => {
+  if (!codeBlock) return
+
+  const mdGrammer = Prism.languages.markdown
+  if (!mdGrammer) {
+    console.error('Markdown grammar not found')
+    console.log(Prism.languages)
+    return
+  }
+  const tokens = Prism.tokenize(content, mdGrammer)
+
+  tokenTypes.forEach((tokenType) => {
+    CSS.highlights.get(tokenType)?.clear()
+  })
+
+  paintTokenHighlights(codeBlock, tokens)
+}
+
+const tokenTypes = [
+  'atrule',
+  'attr-name',
+  'attr-value',
+  'bold',
+  'boolean',
+  'builtin',
+  'cdata',
+  'char',
+  'class-name',
+  'comment',
+  'constant',
+  'deleted',
+  'doctype',
+  'entity',
+  'function',
+  'important',
+  'inserted',
+  'italic',
+  'keyword',
+  'namespace',
+  'number',
+  'operator',
+  'prolog',
+  'property',
+  'punctuation',
+  'regex',
+  'rule',
+  'selector',
+  'string',
+  'symbol',
+  'tag',
+  'url',
+]
